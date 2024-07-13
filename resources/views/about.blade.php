dae5ef19bff86be1639041b208062896f46c7965
Zevaldo <chillydudee@gmail.com>
2024-07-13 18:32:09 +0700

    perbaikan final

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,21 +1,4 @@
-<<<<<<< HEAD
 <x-layout>
     <x-slot:title>{{$title}}</x-slot:title>
     <h3>Ini adalah halaman about</h3>
 </x-layout>
-=======
-<x-header></x-header>
-<body class="h-full">
-    <x-navbar></x-navbar>
-    <div class="min-h-full">
-    
-        <main>
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <!-- Your content -->
-            </div>
-        </main>
-    </div>
-    
-    </body>
-</html>
->>>>>>> 40c5445de44f4ce7c42457d594cacd0cf92c0609
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,25 +1,4 @@
-<<<<<<< HEAD
 <x-layout>
     <x-slot:title>{{$title}}</x-slot:title>
     <h3>Ini adalah halaman contact</h3>
 </x-layout>
-=======
-
-<x-header></x-header>
-<body class="h-full">
-    <x-navbar></x-navbar>
-    <div class="min-h-full">
-    
-        <main>
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1>Ini adalah halaman contact</h1>
-                <p>GITHUB: Barbecude</p>
-                <p>INSTAGRAM: URFAVRECIPE</p>
-                <p>EMAIL: zevaldoganteng@gmail.com</p>
-            </div>
-        </main>
-    </div>
-    
-    </body>
-</html>
->>>>>>> 40c5445de44f4ce7c42457d594cacd0cf92c0609
